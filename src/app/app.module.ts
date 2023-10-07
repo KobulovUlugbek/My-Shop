a4df5b0aaa414dd655f144764e76ac370b1a0b70
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-10-07 11:03:21 +0200

    Reactive Forms and Dialog window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,9 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -38,6 +41,12 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatCardModule,
     HttpClientModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import { IProducts } from 'src/app/models/products';
 import { ProductsService } from 'src/app/services/products.service';
+import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
 
 @Component({
   selector: 'app-products',
@@ -12,14 +14,28 @@ export class ProductsComponent implements OnInit {
 
   products: IProducts[];
   productsSubscription: Subscription;
+  canEdit: boolean = false;
+  canView: boolean = false;
 
-  constructor(private ProductsService: ProductsService) {}
+
+  constructor(private ProductsService: ProductsService, public dialog: MatDialog) {}
 
   ngOnInit():void{
+    this.canEdit = true;
+
     this.productsSubscription = this.ProductsService.getProducts().subscribe((data) => {
       this.products = data;
     });
   }
+
+  openDialog(): void {
+    let dialogConfig = new MatDialogConfig;
+    dialogConfig.width = '500px';
+    dialogConfig.disableClose = true;
+
+    const dialogRef = this.dialog.open(DialogBoxComponent, dialogConfig);
+  }
+
 
   ngOnDestroy(){
     if(this.productsSubscription) this.productsSubscription.unsubscribe();
